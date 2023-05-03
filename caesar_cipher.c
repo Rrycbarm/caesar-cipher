656f241c19ee07e31b8c00523b0386538edbaa8d
Rrycbarm <39190632+Rrycbarm@users.noreply.github.com>
2023-05-03 22:36:00 +0200

    Update caesar_cipher.c
    Replaced ValueMutex with FuriMutex to work with newer firmwares.

--- caesar_cipher.c
+++ caesar_cipher.c
@@ -22,6 +22,7 @@ typedef struct {
 
 typedef struct {
   ViewDispatcher* view_dispatcher;
+  FuriMutex* mutex;
   TextInput* text_input;
   TextBox* text_box;
   char input[TEXT_BUFFER_SIZE];
@@ -58,7 +59,8 @@ static void build_output(char* input, char* output) {
 }
 
 static void text_input_callback(void* ctx) {
-  CaesarState* caesar_state = acquire_mutex((ValueMutex*)ctx, 25);
+  CaesarState* caesar_state = ctx;
+  furi_mutex_acquire(caesar_state->mutex, FuriWaitForever);
   FURI_LOG_D("caesar_cipher", "Input text: %s", caesar_state->input);
   // this is where we build the output.
   string_to_uppercase(caesar_state->input);
@@ -66,15 +68,15 @@ static void text_input_callback(void* ctx) {
   build_output(caesar_state->input, caesar_state->output);
   text_box_set_text(caesar_state->text_box, caesar_state->output);
   view_dispatcher_switch_to_view(caesar_state->view_dispatcher, 1);
-
-  release_mutex((ValueMutex*)ctx, caesar_state);
+  furi_mutex_release(caesar_state->mutex);
 }
 
 
 static bool back_event_callback(void* ctx) {
-  const CaesarState* caesar_state = acquire_mutex((ValueMutex*)ctx, 25);
+  CaesarState* caesar_state = ctx;
+  furi_mutex_acquire(caesar_state->mutex, FuriWaitForever);
   view_dispatcher_stop(caesar_state->view_dispatcher);
-  release_mutex((ValueMutex*)ctx, caesar_state);
+  furi_mutex_release(caesar_state->mutex);
   return true;
 }
 
@@ -82,6 +84,7 @@ static void caesar_cipher_state_init(CaesarState* const caesar_state) {
   caesar_state->view_dispatcher = view_dispatcher_alloc();
   caesar_state->text_input = text_input_alloc();
   caesar_state->text_box = text_box_alloc();
+  caesar_state->mutex = furi_mutex_alloc(FuriMutexTypeNormal);
   text_box_set_font(caesar_state->text_box, TextBoxFontText);
 }
 
@@ -91,6 +94,7 @@ static void caesar_cipher_state_free(CaesarState* const caesar_state) {
   view_dispatcher_remove_view(caesar_state->view_dispatcher, 0);
   view_dispatcher_remove_view(caesar_state->view_dispatcher, 1);
   view_dispatcher_free(caesar_state->view_dispatcher);
+  furi_mutex_free(caesar_state->mutex);
   free(caesar_state);
 }
 
@@ -101,8 +105,7 @@ int32_t caesar_cipher_app() {
   FURI_LOG_D("caesar_cipher", "Running caesar_cipher_state_init");
   caesar_cipher_state_init(caesar_state);
 
-  ValueMutex state_mutex;
-  if(!init_mutex(&state_mutex, caesar_state, sizeof(CaesarState))) {
+  if(!caesar_state->mutex) { 
     FURI_LOG_E("caesar_cipher", "cannot create mutex\r\n");
     free(caesar_state);
     return 255;
@@ -112,7 +115,7 @@ int32_t caesar_cipher_app() {
   text_input_set_result_callback(
     caesar_state->text_input,
     text_input_callback,
-    &state_mutex,
+    caesar_state,
     caesar_state->input,
     TEXT_BUFFER_SIZE,
     //clear default text
@@ -134,12 +137,11 @@ int32_t caesar_cipher_app() {
   view_dispatcher_attach_to_gui(caesar_state->view_dispatcher, gui, ViewDispatcherTypeFullscreen);
   FURI_LOG_D("ceasar_cipher", "starting view dispatcher");
   view_dispatcher_set_navigation_event_callback(caesar_state->view_dispatcher, back_event_callback);
-  view_dispatcher_set_event_callback_context(caesar_state->view_dispatcher, &state_mutex);
+  view_dispatcher_set_event_callback_context(caesar_state->view_dispatcher, caesar_state);
   view_dispatcher_switch_to_view(caesar_state->view_dispatcher, 0);
   view_dispatcher_run(caesar_state->view_dispatcher);
 
   furi_record_close("gui");
-  delete_mutex(&state_mutex);
   caesar_cipher_state_free(caesar_state);
 
   return 0;
